ea41b87fe03e370aa0b72a1cc7a640e66446eda9
UncleBTech <unclebobtechnologies@gmail.com>
2026-04-24 01:29:14 +0100

    Fix string quotes in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import tailwindcss from "@tailwindcss/vite";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+    TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
     react(),
     tailwindcss(),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   build: {
-    outDir: "dist",
+    outDir: 'dist',
   },
-  server: {
-    host: "::",
-    port: 8080,
-  },
-});
+})
